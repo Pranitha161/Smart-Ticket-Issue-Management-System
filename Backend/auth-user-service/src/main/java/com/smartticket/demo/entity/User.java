004754968535c6af7a08aac0f00f89b7f126e6bb
Pranitha <pranithareddy160805@gmail.com>
2025-12-27 21:45:02 +0530

    Added user entity

---
+++ Backend/auth-user-service/src/main/java/com/smartticket/demo/entity/User.java
@@ -0,0 +1,34 @@
+package com.smartticket.demo.entity;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+	@Id
+	private String id;
+	@NotBlank 
+	private String username;
+	@NotBlank
+	private String password;
+	@Email 
+	private String email;
+	private Set<String> roles; 
+	private boolean enabled = true;
+	private LocalDateTime  createdAt; 
+	private LocalDateTime  updatedAt;
+}

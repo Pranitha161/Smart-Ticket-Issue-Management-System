1cd5ddb24655b46f89a1f515526a778568e4da55
Pranitha <pranithareddy160805@gmail.com>
2025-12-27 22:45:22 +0530

    Added entities

---
+++ Backend/auth-user-service/src/main/java/com/smartticket/demo/entity/ApiResponse.java
@@ -0,0 +1,20 @@
+package com.smartticket.demo.entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+
+	private String status;
+	private int code;
+	private String message;
+	private T data;
+
+}

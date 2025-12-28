fe0f18ffdb3dd4f906f6eb5d46946c55516ebbe7
Pranitha <pranithareddy160805@gmail.com>
2025-12-28 15:10:32 +0530

    Added repository

---
+++ Backend/ticket-service/src/main/java/com/smartticket/demo/repository/TicketRepository.java
@@ -0,0 +1,14 @@
+package com.smartticket.demo.repository;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import com.smartticket.demo.entity.Ticket;
+
+import reactor.core.publisher.Mono;
+
+public interface TicketRepository extends ReactiveMongoRepository<Ticket, String>{
+	
+	Mono<Ticket> findCreatedBy(String userId);
+	
+	Mono<Ticket> findAssignedTo(String agentId);
+}

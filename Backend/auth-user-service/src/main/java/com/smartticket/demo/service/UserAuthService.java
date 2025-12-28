567ddeafb93c90ef2049e85e8b888359cc0e9ebb
Pranitha <pranithareddy160805@gmail.com>
2025-12-28 10:15:55 +0530

    Added all the servies according to controller paths

--- Backend/auth-user-service/src/main/java/com/smartticket/demo/service/UserAuthService.java
+++ Backend/auth-user-service/src/main/java/com/smartticket/demo/service/UserAuthService.java
@@ -1,29 +1,39 @@
 package com.smartticket.demo.service;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 
 import com.smartticket.demo.entity.ApiResponse;
+import com.smartticket.demo.entity.AuthResponse;
 import com.smartticket.demo.entity.LoginRequest;
+import com.smartticket.demo.entity.SimpleApiResponse;
 import com.smartticket.demo.entity.User;
 
 import reactor.core.publisher.Mono;
 
 public interface UserAuthService {
 	
-	Mono<ResponseEntity<ApiResponse<User>>> getUsers();
+	Mono<ResponseEntity<SimpleApiResponse>> login(LoginRequest request);
 	
-	Mono<ResponseEntity<ApiResponse<Void>>> login(LoginRequest request);
+	Mono<ResponseEntity<SimpleApiResponse>> register(User user);
 	
-	Mono<ResponseEntity<ApiResponse<Void>>> register(User user);
+	Mono<ResponseEntity<ApiResponse<List<User>>>> getUsers();
 	
-	Mono<ResponseEntity<ApiResponse<User>>> getUserById(String id);
+	Mono<ResponseEntity<AuthResponse>> getUserById(String id);
 	
-	Mono<ResponseEntity<ApiResponse<Void>>> updateUserById(String id);
+	Mono<ResponseEntity<AuthResponse>> getUserByEmail(String email);
 	
-	Mono<ResponseEntity<ApiResponse<Void>>> deletetUserById(String id);
+	Mono<ResponseEntity<SimpleApiResponse>> updateUserById(String id,AuthResponse user);
 	
-//	Mono<ResponseEntity<ApiResponse<Void>>> assignRole(String )
+	Mono<ResponseEntity<SimpleApiResponse>> deletetUserById(String id);
+
+	Mono<ResponseEntity<SimpleApiResponse>> requestPasswordReset(String email);
+
+	Mono<ResponseEntity<SimpleApiResponse>> resetPassword(String email, String token);
+
+	Mono<ResponseEntity<SimpleApiResponse>> changePassword(String userName, String oldPassword, String newPassword);
 	
-	
+	Mono<ResponseEntity<ApiResponse<User>>> me(); 
 
 }

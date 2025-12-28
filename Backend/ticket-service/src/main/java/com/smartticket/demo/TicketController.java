9f4555f70b9f6da39368860225b1a719a07bdc63
Pranitha <pranithareddy160805@gmail.com>
2025-12-28 15:48:50 +0530

    Added autowired

--- Backend/ticket-service/src/main/java/com/smartticket/demo/TicketController.java
+++ Backend/ticket-service/src/main/java/com/smartticket/demo/TicketController.java
@@ -1,5 +1,6 @@
 package com.smartticket.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.smartticket.demo.entity.ApiResponse;
@@ -17,10 +19,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
-@RestController("/tickets")
+@RestController
+@RequestMapping("/tickets")
 @RequiredArgsConstructor
 public class TicketController {
 	
+	@Autowired
 	private TicketServiceImplementation ticketService;
 	
 	@PostMapping("/create")

7125e6a2f45f39ab47e1f3fc8f3cedc0caf798e7
Pranitha <pranithareddy160805@gmail.com>
2025-12-28 17:03:21 +0530

    added @Repository

--- Backend/notification-service/src/main/java/com/smartticket/demo/repository/NotificationRepository.java
+++ Backend/notification-service/src/main/java/com/smartticket/demo/repository/NotificationRepository.java
@@ -1,9 +1,11 @@
 package com.smartticket.demo.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.smartticket.demo.entity.Notification;
 
+@Repository
 public interface NotificationRepository extends ReactiveMongoRepository<Notification, String>{
 
 }

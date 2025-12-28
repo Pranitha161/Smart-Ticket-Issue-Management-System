6432e4dec631970bd1c2e522bf3da255568d3341
Pranitha <pranithareddy160805@gmail.com>
2025-12-28 17:03:01 +0530

    Added @Repository

--- Backend/auth-user-service/src/main/java/com/smartticket/demo/repository/UserAuthRepository.java
+++ Backend/auth-user-service/src/main/java/com/smartticket/demo/repository/UserAuthRepository.java
@@ -1,11 +1,13 @@
 package com.smartticket.demo.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.smartticket.demo.entity.User;
 
 import reactor.core.publisher.Mono;
 
+@Repository
 public interface UserAuthRepository extends ReactiveMongoRepository<User, String> {
 	
 	Mono<User> findByEmail(String email);

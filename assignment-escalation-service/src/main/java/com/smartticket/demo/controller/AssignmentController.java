624089759db33794a414dd90e91e00765ff461a8
Pranitha <pranithareddy160805@gmail.com>
2025-12-29 20:12:11 +0530

    Added controllers for summary report

--- assignment-escalation-service/src/main/java/com/smartticket/demo/controller/AssignmentController.java
+++ assignment-escalation-service/src/main/java/com/smartticket/demo/controller/AssignmentController.java
@@ -1,12 +1,22 @@
 package com.smartticket.demo.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.smartticket.demo.dto.AgentSummaryDto;
+import com.smartticket.demo.dto.EscalationSummaryDto;
 import com.smartticket.demo.entity.ApiResponse;
 import com.smartticket.demo.entity.Assignment;
 import com.smartticket.demo.enums.PRIORITY;
-import com.smartticket.demo.service.AssignmentService;
+import com.smartticket.demo.service.implementation.AssignmentServiceImplementation;
 
 import reactor.core.publisher.Mono;
 
@@ -14,9 +24,9 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/assignments")
 public class AssignmentController {
 
-	private final AssignmentService assignmentService;
+	private final AssignmentServiceImplementation assignmentService;
 
-	public AssignmentController(AssignmentService assignmentService) {
+	public AssignmentController(AssignmentServiceImplementation assignmentService) {
 		this.assignmentService = assignmentService;
 	}
 
@@ -63,5 +73,15 @@ public class AssignmentController {
 	public Mono<ResponseEntity<Assignment>> autoAssign(@PathVariable String ticketId) {
 		return assignmentService.autoAssign(ticketId).map(ResponseEntity::ok)
 				.onErrorResume(e -> Mono.just(ResponseEntity.badRequest().build()));
+	}
+
+	@GetMapping("/agent-summary")
+	public Mono<ResponseEntity<List<AgentSummaryDto>>> getAgentWorkloadSummary() {
+		return assignmentService.assignmentsPerAgent().map(ResponseEntity::ok);
+	}
+
+	@GetMapping("/escalations/summary")
+	public Mono<ResponseEntity<List<EscalationSummaryDto>>> getEscalationSummary() {
+		return assignmentService.getEscalationSummary().map(ResponseEntity::ok);
 	}
 }

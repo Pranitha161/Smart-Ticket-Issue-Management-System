d0a74d767c6628c823443f8b94f2917000605c4f
Pranitha <pranithareddy160805@gmail.com>
2025-12-29 10:15:59 +0530

    Removed ResponseEntity

--- Backend/ticket-service/src/main/java/com/smartticket/demo/service/TicketService.java
+++ Backend/ticket-service/src/main/java/com/smartticket/demo/service/TicketService.java
@@ -1,28 +1,26 @@
 package com.smartticket.demo.service;
 
-import org.springframework.http.ResponseEntity;
-
-import com.smartticket.demo.entity.ApiResponse;
 import com.smartticket.demo.entity.Ticket;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface TicketService {
-	
-	Mono<ResponseEntity<ApiResponse>> createTicket(Ticket ticket);
-	
-	Mono<ResponseEntity<Ticket>> getTicketById(String id);
-	
-	Mono<ResponseEntity<ApiResponse>> updateTicketById(String id,Ticket updateTicket);
-	
-	Mono<ResponseEntity<ApiResponse>> closeTicket(String id);
-	
-	Mono<ResponseEntity<ApiResponse>> reopenTicket(String id);
-	
-	Mono<ResponseEntity<ApiResponse>> resolveTicket(String id);
 
-	Mono<ResponseEntity<ApiResponse>> deleteTicket(String id);
-	
-	
+	Mono<Ticket> createTicket(Ticket ticket);
+
+	Mono<Ticket> getTicketById(String id);
+
+	Mono<Ticket> updateTicketById(String id, Ticket updatedTicket);
+
+	Mono<Ticket> closeTicket(String id);
+
+	Mono<Ticket> reopenTicket(String id);
+
+	Mono<Ticket> resolveTicket(String id);
+
+	Mono<Void> deleteTicket(String id);
+
+	Flux<Ticket> getAllTickets();
 
 }

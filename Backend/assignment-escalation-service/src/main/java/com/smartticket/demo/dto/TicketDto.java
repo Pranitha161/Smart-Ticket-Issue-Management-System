370d57a9ee11bf874682a8dd71131b01238f9b49
Pranitha <pranithareddy160805@gmail.com>
2025-12-29 12:39:42 +0530

    Added dtos

---
+++ Backend/assignment-escalation-service/src/main/java/com/smartticket/demo/dto/TicketDto.java
@@ -0,0 +1,16 @@
+package com.smartticket.demo.dto;
+
+import com.smartticket.demo.enums.PRIORITY;
+import com.smartticket.demo.enums.STATUS;
+
+import lombok.Data;
+
+@Data 
+public class TicketDto { 
+	
+	private String id; 
+	private PRIORITY priority; 
+	private STATUS status; 
+	private String categoryId; 
+	
+}

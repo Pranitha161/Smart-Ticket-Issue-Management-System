09aa7299fccb4615ab39ac5bb25288d7b37747ae
Pranitha <pranithareddy160805@gmail.com>
2025-12-29 12:42:03 +0530

    Added some responses in repository

--- Backend/assignment-escalation-service/src/main/java/com/smartticket/demo/repository/AssignmentRepository.java
+++ Backend/assignment-escalation-service/src/main/java/com/smartticket/demo/repository/AssignmentRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.smartticket.demo.entity.Assignment;
+import com.smartticket.demo.enums.ASSIGNMENT_STATUS;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface AssignmentRepository extends ReactiveMongoRepository<Assignment, String> {
@@ -17,4 +19,8 @@ public interface AssignmentRepository extends ReactiveMongoRepository<Assignment
 	Flux<Assignment> findByAgentId(String agentId);
 
 	Flux<Assignment> findByTicketId(String ticketId);
+
+	Flux<Assignment> findByStatus(ASSIGNMENT_STATUS status);
+
+	Mono<Assignment> findTopByTicketIdOrderByAssignedAtDesc(String ticketId);
 }

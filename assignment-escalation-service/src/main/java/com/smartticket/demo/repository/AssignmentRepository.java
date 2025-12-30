6cf238d356c44123a426a96cca7b50e6ab12fb72
Pranitha <pranithareddy160805@gmail.com>
2025-12-30 08:22:33 +0530

    Added aggregations

--- assignment-escalation-service/src/main/java/com/smartticket/demo/repository/AssignmentRepository.java
+++ assignment-escalation-service/src/main/java/com/smartticket/demo/repository/AssignmentRepository.java
@@ -2,9 +2,11 @@ package com.smartticket.demo.repository;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
+import com.smartticket.demo.dto.EscalationSummaryDto;
 import com.smartticket.demo.entity.Assignment;
 import com.smartticket.demo.enums.ASSIGNMENT_STATUS;
 
@@ -23,4 +25,8 @@ public interface AssignmentRepository extends ReactiveMongoRepository<Assignment
 	Flux<Assignment> findByStatus(ASSIGNMENT_STATUS status);
 
 	Mono<Assignment> findTopByTicketIdOrderByAssignedAtDesc(String ticketId);
+
+	@Aggregation(pipeline = { "{ $group: { _id: '$escalationLevel', count: { $sum: 1 } } }",
+			"{ $project: { level: '$_id', count: 1, _id: 0 } }" })
+	Flux<EscalationSummaryDto> getEscalationSummary();
 }

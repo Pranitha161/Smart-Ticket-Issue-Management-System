654b2df77b2ef626d6dcc89ce745b59e481880bb
Pranitha <pranithareddy160805@gmail.com>
2025-12-30 16:31:22 +0530

    Restricted the dasboard to only manager and admin roles

--- src/app/modules/dashboard/dashboard-routing-module.ts
+++ src/app/modules/dashboard/dashboard-routing-module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { roleGuard } from '../../core/guards/role-guard';
+import { Dashboard } from './dashboard/dashboard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  
+    component: Dashboard, 
+    canActivate: [roleGuard], 
+    data: { roles: ['ADMIN', 'MANAGER'] } }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

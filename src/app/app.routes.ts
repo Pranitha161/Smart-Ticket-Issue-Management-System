ffd013af7915a0ed095875ad4871a930035ffd65
Pranitha <pranithareddy160805@gmail.com>
2025-12-30 22:55:48 +0530

    Added routes related to login and signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {
+        path: 'signup',
+        loadComponent: () => import('../app/modules/auth/signup/signup').then(m => m.Signup),
+    },
+    {
+        path:'login',
+        loadComponent:()=>import('../app/modules/auth/login/login').then(m=>m.Login)
+    }
+];

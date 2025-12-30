0479980fa074e6808fbee0d1dc3831f7e10b1b08
Pranitha <pranithareddy160805@gmail.com>
2025-12-30 23:06:29 +0530

    Added logic to call authservice and display first letter of logged in user

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  username: string | null = null;
+  constructor(private authService: AuthService) { }
+  ngOnInit() {
+    if (this.authService.isLoggedIn()) {
+      this.username = this.authService.getUsername();
+    }
+  }
+  get firstLetter(): string { return this.username ? this.username.charAt(0).toUpperCase() : ''; }
 
 }

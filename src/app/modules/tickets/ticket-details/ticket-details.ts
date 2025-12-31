f7db620d63946669b8d67dfb9dd19395b207c6d0
Pranitha <pranithareddy160805@gmail.com>
2025-12-31 13:50:40 +0530

    Added ticket-details component

---
+++ src/app/modules/tickets/ticket-details/ticket-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-details',
+  imports: [],
+  templateUrl: './ticket-details.html',
+  styleUrl: './ticket-details.css',
+})
+export class TicketDetails {
+
+}

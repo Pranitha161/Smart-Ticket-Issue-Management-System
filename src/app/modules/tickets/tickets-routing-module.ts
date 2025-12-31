7b1d6b7547b9648eb2c6bfb95cfa4ac2fab51d12
Pranitha <pranithareddy160805@gmail.com>
2025-12-31 13:53:08 +0530

    Added routing details of tickets

--- src/app/modules/tickets/tickets-routing-module.ts
+++ src/app/modules/tickets/tickets-routing-module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TicketList } from './ticket-list/ticket-list';
 
-const routes: Routes = [];
-
+const routes: Routes = [ 
+  { path: '', component: TicketList }, 
+   { path: 'create', loadComponent: () => import('./ticket-form/ticket-form').then(m => m.TicketForm) }
+  //  { path: ':id', component: TicketDetailComponent } 
+  ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]

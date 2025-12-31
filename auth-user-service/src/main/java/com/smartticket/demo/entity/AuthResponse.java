0b33a99895562af0e0f3cf2d3b9ae8e8f485d53c
Pranitha <pranithareddy160805@gmail.com>
2025-12-31 23:48:05 +0530

    Added displayId

--- auth-user-service/src/main/java/com/smartticket/demo/entity/AuthResponse.java
+++ auth-user-service/src/main/java/com/smartticket/demo/entity/AuthResponse.java
@@ -11,7 +11,9 @@ import lombok.Data;
 @AllArgsConstructor
 public class AuthResponse {
 	private String id;
+	private String displayId;
 	private String email;
 	private String username;
+	private boolean enabled;
 	private Set<ROLE> roles;
 }

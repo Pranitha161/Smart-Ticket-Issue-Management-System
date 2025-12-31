17b32ccb66801ebea40527a4c7b12f3e12f27323
Pranitha <pranithareddy160805@gmail.com>
2025-12-31 23:48:46 +0530

    Added aggregations

--- auth-user-service/src/main/java/com/smartticket/demo/repository/UserAuthRepository.java
+++ auth-user-service/src/main/java/com/smartticket/demo/repository/UserAuthRepository.java
@@ -1,10 +1,15 @@
 package com.smartticket.demo.repository;
 
+import java.util.List;
+
+import org.bson.Document;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.smartticket.demo.entity.User;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
@@ -15,5 +20,21 @@ public interface UserAuthRepository extends ReactiveMongoRepository<User, String
 	Mono<User> findByUsername(String username);
 	
 	Mono<User> findByResetToken(String token);
+	
+	@Aggregation(pipeline = { "{ $count: 'totalUsers' }" })
+	Flux<Document> countTotalUsers();
+	
+	@Aggregation(pipeline = {
+			  "{ $match: { enabled: true } }",
+			  "{ $count: 'activeUsers' }"
+			})
+			Flux<Document> countActiveUsers();
+	@Aggregation(pipeline = {
+			  "{ $unwind: '$roles' }",
+			  "{ $group: { _id: '$roles', count: { $sum: 1 } } }"
+			})
+			Flux<Document> countUsersByRole();
+
+
 
 }

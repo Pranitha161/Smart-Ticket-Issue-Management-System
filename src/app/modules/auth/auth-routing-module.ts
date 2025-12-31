8ec08c287acf78ca2de78bf8ad3757a1f97a7cf7
Pranitha <pranithareddy160805@gmail.com>
2025-12-31 20:58:25 +0530

    Added signup,login, admin-panel and other paths

--- src/app/modules/auth/auth-routing-module.ts
+++ src/app/modules/auth/auth-routing-module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {
+        path: 'signup',
+        loadComponent: () => import('./signup/signup').then(m => m.Signup),
+    },
+    {
+        path:'login',
+        loadComponent:()=>import('./login/login').then(m=>m.Login)
+    },
+     {
+        path:'admin-panel',
+        loadComponent:()=>import('./admin-panel/admin-panel').then(m=>m.AdminPanel)
+    },
+    {
+      path:'category-panel',
+      loadComponent:()=>import('./category-panel/category-panel').then(m=>m.CategoryPanel)
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {
+  
+ }

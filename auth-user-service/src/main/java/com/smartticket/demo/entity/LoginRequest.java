2d6fd59c96734a4ac7a89232e5c6d9ddb0975290
Pranitha <pranithareddy160805@gmail.com>
2025-12-31 23:48:27 +0530

    Added validations

--- auth-user-service/src/main/java/com/smartticket/demo/entity/LoginRequest.java
+++ auth-user-service/src/main/java/com/smartticket/demo/entity/LoginRequest.java
@@ -1,10 +1,15 @@
 package com.smartticket.demo.entity;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LoginRequest {
-	
+	@Email
 	private String email;
+	@NotBlank
 	private String password;
 }

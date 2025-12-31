0b55d1eafd2415c8feb67eff0652fe43fd43e606
Pranitha <pranithareddy160805@gmail.com>
2025-12-31 20:56:09 +0530

    Added auth module but adding auth path and loaded the childrens

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,8 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: '', redirectTo: 'tickets', pathMatch: 'full' },
     {
-        path: 'signup',
-        loadComponent: () => import('../app/modules/auth/signup/signup').then(m => m.Signup),
-    },
-    {
-        path:'login',
-        loadComponent:()=>import('../app/modules/auth/login/login').then(m=>m.Login)
+        path: 'auth',
+        loadChildren: () => import('./modules/auth/auth-module').then(m => m.AuthModule),
     },
     { path: 'tickets', loadChildren: () => import('./modules/tickets/tickets-module').then(m => m.TicketsModule) }
 ];

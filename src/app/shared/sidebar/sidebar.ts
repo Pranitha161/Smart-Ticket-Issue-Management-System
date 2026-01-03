59de8bea3a38029a6188283e4865d9746eee3a87
Pranitha <pranithareddy160805@gmail.com>
2026-01-04 00:31:19 +0530

    Added loggedin to act accordingly when user logout

--- src/app/shared/sidebar/sidebar.ts
+++ src/app/shared/sidebar/sidebar.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, inject } from '@angular/core'; // Added computed and inject
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../core/services/auth';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterModule,CommonModule],
+  standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar implements OnInit{
-  isLoggedIn = false;
-  constructor(private authService: AuthService) {}
-  ngOnInit(): void { 
-     this.isLoggedIn  = this.authService.isLoggedIn();
-  }
+export class Sidebar {
+  private authService = inject(AuthService);
+
+  isLoggedIn = computed(() => this.authService.loggedIn());
 
 }

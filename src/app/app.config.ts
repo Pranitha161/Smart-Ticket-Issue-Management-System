1391f4dc0524d09dd5ccec4633c0ccc18715b383
Pranitha <pranithareddy160805@gmail.com>
2026-01-04 00:08:02 +0530

    Added toast service and error handler

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,36 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+// import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+// import { provideRouter } from '@angular/router';
 
+// import { routes } from './app.routes';
+// import { provideHttpClient, withInterceptors } from '@angular/common/http';
+// import { JwtInterceptor } from './core/interceptors/jwt-interceptor';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideBrowserGlobalErrorListeners(),
+//     provideRouter(routes),
+//     provideHttpClient( withInterceptors([JwtInterceptor])),
+//   ]
+// };
+// src/app/app.config.ts
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideAppInitializer, inject } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { JwtInterceptor } from './core/interceptors/jwt-interceptor';
+import { LookupService } from './core/services/lookup-service';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { errorInterceptor } from './core/interceptors/error-interceptor'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient( withInterceptors([JwtInterceptor]))
+    provideHttpClient(withInterceptors([JwtInterceptor,errorInterceptor])),
+    provideAppInitializer(() => {
+      const lookup = inject(LookupService);
+      return lookup.init();
+    })
   ]
 };

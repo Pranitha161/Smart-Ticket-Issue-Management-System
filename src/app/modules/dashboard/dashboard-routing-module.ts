3258429895caa794e85724958d9ee6fcff2f7255
Pranitha <pranithareddy160805@gmail.com>
2026-01-04 00:40:16 +0530

    Added paths

--- src/app/modules/dashboard/dashboard-routing-module.ts
+++ src/app/modules/dashboard/dashboard-routing-module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { roleGuard } from '../../core/guards/role-guard';
 import { Dashboard } from './dashboard/dashboard';
+import { Report } from './report/report';
 
 const routes: Routes = [
   { path: '',  
     component: Dashboard, 
     // canActivate: [roleGuard], 
     // data: { roles: ['ADMIN', 'MANAGER'] }
+   }
+   ,{
+    path:'report',
+    component:Report
    }
 ];
 

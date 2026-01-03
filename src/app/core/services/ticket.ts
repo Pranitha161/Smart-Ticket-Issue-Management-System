39b195c28cfe600fcc37da318ebafd6212e3d305
Pranitha <pranithareddy160805@gmail.com>
2026-01-04 00:15:38 +0530

    Added new paths

--- src/app/core/services/ticket.ts
+++ src/app/core/services/ticket.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PrioritySummaryDto, StatusSummaryDto, Ticket, UserTicketStats } from '../../shared/models/ticket.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class TicketService {
-  private baseUrl = 'http://localhost:8765/ticket-service/tickets';
+  private baseUrl =   `${environment.apiGatewayUrl}${environment.endpoints.tickets}/tickets`;
 
   constructor(private http: HttpClient) { }
 
@@ -45,14 +46,6 @@ export class TicketService {
     return this.http.delete(`${this.baseUrl}/${id}`);
   }
 
-  getTicketActivity(ticketId: string): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/${ticketId}/activity`);
-  }
-
-  addTicketComment(ticketId: string, actorId: string, comment: string): Observable<any> {
-    return this.http.post<any>(`${this.baseUrl}/${ticketId}/activity/comment`, null, { params: { actorId, comment } });
-  }
-
   getUserStats(userId: string): Observable<UserTicketStats> {
     return this.http.get<UserTicketStats>(`${this.baseUrl}/user/${userId}/stats`);
   }
@@ -79,6 +72,15 @@ export class TicketService {
   getRecentTicketsByAgent(agentId: string): Observable<Ticket[]> {
     return this.http.get<Ticket[]>(`${this.baseUrl}/recent/agent/${agentId}`);
   }
+
+  getTicketsByAgentId(agentId: string): Observable<Ticket[]> {
+  return this.http.get<Ticket[]>(`${this.baseUrl}/agent/${agentId}`);
+}
+
+getAgentStats(agentId: string): Observable<UserTicketStats> {
+    return this.http.get<UserTicketStats>(`${this.baseUrl}/agent/${agentId}/stats`);
+  }
+
 
 
 }

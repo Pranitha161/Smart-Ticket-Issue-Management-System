dcc4da47f078c6b84fa8a04a3b1442dee7d2040c
Pranitha <pranithareddy160805@gmail.com>
2026-01-04 00:14:11 +0530

    Modified logic by injecting lookup service and also added toast for messages

--- src/app/core/services/category.ts
+++ src/app/core/services/category.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { environment } from "../../../environments/environment";
 
 export interface CategoryDto {
   id?: string;
@@ -12,16 +13,17 @@ export interface CategoryDto {
 
 @Injectable({ providedIn: 'root' })
 export class Category {
-  private baseUrl = 'http://localhost:8765/ticket-service/categories';
 
-  constructor(private http: HttpClient) {}
+  private baseUrl = `${environment.apiGatewayUrl}${environment.endpoints.tickets}/categories`;
 
-  getAllCategories(): Observable<CategoryDto[]> {
+  constructor(private http: HttpClient) { }
+
+  getAllCategories(options: any = {}): Observable<CategoryDto[]> {
     return this.http.get<CategoryDto[]>(this.baseUrl);
   }
 
   createCategory(category: CategoryDto): Observable<CategoryDto> {
-    return this.http.post<CategoryDto>(this.baseUrl, category);
+    return this.http.post<CategoryDto>(`${this.baseUrl}/create`, category);
   }
 
   updateCategory(id: string, category: CategoryDto): Observable<CategoryDto> {

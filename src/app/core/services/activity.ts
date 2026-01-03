32b524db210b11b77f45231e91d0f4103b663bf8
Pranitha <pranithareddy160805@gmail.com>
2026-01-04 00:12:17 +0530

    Modified the file

--- src/app/core/services/activity.ts
+++ src/app/core/services/activity.ts
@@ -1,8 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Activity {
+  private baseUrl = 'http://localhost:8765/ticket-service/tickets/${ticketId}/activity';
+
+  constructor(private http: HttpClient) { }
+
+  getTicketActivity(ticketId: string): Observable<any[]> {
+      return this.http.get<any[]>(`${this.baseUrl}`);
+    }
   
+    logActivity(ticketId: string, actorId: string, comment: string): Observable<any> {
+      return this.http.post<any>(`${this.baseUrl}/${ticketId}/comment`, null, { params: { actorId, comment } });
+    }
 }

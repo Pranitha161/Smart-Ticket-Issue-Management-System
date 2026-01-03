4478d3a6475542aa1bff64ab88a62b816659cab5
Pranitha <pranithareddy160805@gmail.com>
2026-01-04 00:10:18 +0530

    Added toast component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/navbar/navbar';
 import { Sidebar } from './shared/sidebar/sidebar';
+import { ToastComponent } from './shared/components/toast/toast';
+import { AuthService } from './core/services/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,Sidebar],
+  imports: [RouterOutlet,
+    CommonModule,
+    ToastComponent,
+    Navbar,Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  private auth = inject(AuthService);
+  isLoggedIn = computed(() => this.auth.loggedIn());
   protected readonly title = signal('smart-ticket-frontend');
 }

32be000b19e06923827734763bcfe92d4415d4e0
Pranitha <pranithareddy160805@gmail.com>
2026-01-05 01:54:11 +0530

    Added new paths

--- Backend/dashboard-service/src/main/java/com/smartticket/demo/feign/TicketClient.java
+++ Backend/dashboard-service/src/main/java/com/smartticket/demo/feign/TicketClient.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.smartticket.demo.dto.CategorySummaryDto;
 import com.smartticket.demo.dto.PrioritySummaryDto;
 import com.smartticket.demo.dto.StatusSummaryDto;
+import com.smartticket.demo.dto.UserTicketStatsDto;
 
 @FeignClient(name = "ticket-service")
 public interface TicketClient {
@@ -19,4 +21,13 @@ public interface TicketClient {
 
 	@GetMapping("/tickets/category-summary")
 	List<CategorySummaryDto> getCategorySummary();
+	
+	@GetMapping("/tickets/user/{userId}/stats")
+	UserTicketStatsDto getUserStats(@PathVariable String userId);
+	
+	@GetMapping("/tickets/agent/{agentId}/stats")
+	UserTicketStatsDto getAgentStats(@PathVariable String agentId);
+	
+	@GetMapping("/tickets/global-stats")
+	UserTicketStatsDto getGlobalStats();
 }

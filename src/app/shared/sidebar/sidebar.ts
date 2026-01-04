29f70a56a6a9c16dd1cce52304c279d008ac71eb
Pranitha <pranithareddy160805@gmail.com>
2026-01-04 21:00:23 +0530

    Formatted file

--- src/app/shared/sidebar/sidebar.ts
+++ src/app/shared/sidebar/sidebar.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from '@angular/core'; // Added computed and inject
+import { Component, computed, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../core/services/auth';
 import { CommonModule } from '@angular/common';
@@ -14,12 +14,9 @@ export class Sidebar {
 
   private authService = inject(AuthService);
 
-  isLoggedIn = computed(() => {this.authService.loggedIn();});
+  isLoggedIn = computed(() => { this.authService.loggedIn(); });
   userRole = computed(() => {
-    // 1. First, call the signal as a function to get the string[]
-    const currentRoles = this.authService.roles(); 
-
-    // 2. Now you can check the length and access index [0] on the array
+    const currentRoles = this.authService.roles();
     return currentRoles && currentRoles.length > 0 ? currentRoles[0] : null;
   });
 

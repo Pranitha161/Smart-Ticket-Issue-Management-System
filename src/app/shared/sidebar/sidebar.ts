8f708f11d77e8684c130b022d53d4bb4ad3e4b6a
Pranitha <pranithareddy160805@gmail.com>
2026-01-04 20:43:54 +0530

    Added useRole function to show elements in side bar according to role

--- src/app/shared/sidebar/sidebar.ts
+++ src/app/shared/sidebar/sidebar.ts
@@ -11,8 +11,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+
   private authService = inject(AuthService);
 
-  isLoggedIn = computed(() => this.authService.loggedIn());
+  isLoggedIn = computed(() => {this.authService.loggedIn();});
+  userRole = computed(() => {
+    // 1. First, call the signal as a function to get the string[]
+    const currentRoles = this.authService.roles(); 
+
+    // 2. Now you can check the length and access index [0] on the array
+    return currentRoles && currentRoles.length > 0 ? currentRoles[0] : null;
+  });
 
 }

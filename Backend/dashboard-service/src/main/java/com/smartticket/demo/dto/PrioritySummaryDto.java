bd224edfb5eb90880b6e4dcc0e3deae9928e6091
Pranitha <pranithareddy160805@gmail.com>
2026-01-06 00:07:34 +0530

    Added builders

--- Backend/dashboard-service/src/main/java/com/smartticket/demo/dto/PrioritySummaryDto.java
+++ Backend/dashboard-service/src/main/java/com/smartticket/demo/dto/PrioritySummaryDto.java
@@ -2,10 +2,12 @@ package com.smartticket.demo.dto;
 
 import com.smartticket.demo.enums.PRIORITY;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 @Builder
 public class PrioritySummaryDto {
 	private PRIORITY priority;
--- Backend/dashboard-service/src/main/java/com/smartticket/demo/dto/StatusSummaryDto.java
+++ Backend/dashboard-service/src/main/java/com/smartticket/demo/dto/StatusSummaryDto.java
@@ -2,10 +2,12 @@ package com.smartticket.demo.dto;
 
 import com.smartticket.demo.enums.STATUS;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 @Builder
 public class StatusSummaryDto {
 	private STATUS status;
